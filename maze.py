661399f8fc834a55fe5455082efd6e83d3670554
blubbers122 <blubbers122@users.noreply.github.com>
2020-10-01 16:11:28 -0500

    maze is solvable with BFS algorithm, grid resizing and clearing buttons added

--- maze.py
+++ maze.py
@@ -1,9 +1,64 @@
 from util import StackFrontier, QueueFrontier, Node
 
 class Maze():
-    def __init__(self, grid_size, walls):
+    def __init__(self, grid_size, start, end, walls):
         self.grid_size = grid_size
+        self.start = tuple(start)
+        self.end = tuple(end)
         self.walls = walls
+        self.frontier = QueueFrontier()
+
+    def get_path(self, node):
+        path = []
+
+        while node.parent:
+            path.append(node.state)
+            node = node.parent
+
+        return path
 
     def solve_maze(self):
-        print("solving maze")
+        start_node = Node(self.start, None, None)
+        self.frontier.add(start_node)
+
+        explored = set()
+        count = 0
+        while True:
+            if self.frontier.empty():
+                print("couldn't find solution")
+                return [], count
+
+            node = self.frontier.remove()
+            if node.state == self.end:
+                print("found solution")
+                return self.get_path(node), count
+
+            #get node's neighbor cells and adds them to frontier
+            x = node.state[0]
+            y = node.state[1]
+
+            directions = {
+                    "left": (x-1,y),
+                    "right": (x+1,y),
+                    "up": (x,y-1),
+                    "down": (x,y+1)
+                    }
+
+            for direction, index in directions.items():
+
+                cell = None
+
+                if direction == "left" and x > 0:
+                    cell = index
+                if direction == "right" and x < self.grid_size[0] - 1:
+                    cell = index
+                if direction == "up" and y > 0:
+                    cell = index
+                if direction == "down" and y < self.grid_size[1] - 1:
+                    cell = index
+
+                if cell and cell not in explored and not self.frontier.contains_state(cell) and cell not in self.walls:
+                    self.frontier.add(Node(cell, node, direction))
+
+            explored.add(node.state)
+            count += 1
--- run.py
+++ run.py
@@ -5,146 +5,188 @@ from maze import Maze
 
 pygame.init()
 
-size = width, height = 800, 600
-
 black = (0,0,0)
 white = (20,0,155)
 highlight = (200, 40, 0)
+path_color = (0, 200, 230)
 options_box = (150,150,150)
 button_color = (0,60,140)
 start_color = (30, 220, 15)
 end_color = (78, 10, 140)
 
+medium_settings = [30, [20, 20]]
+large_settings = [20, [30, 30]]
+grid_position = (0, 0)
+
+size = width, height = 800, 600
+
 mediumFont = pygame.font.Font("OpenSans-Regular.ttf", 28)
 
-screen = pygame.display.set_mode(size)
+class MazeInterface():
+    def __init__(self):
+        self.cell_size = 30
+        self.grid_size = [20, 20]
+        self.start = [0, 0]
+        self.end = [19, 19]
+        self.empty_cells = []
+        self.filled_cells = []
+        self.wall_indices = []
+        self.buttons = []
+        self.path = []
+        self.count = 0
+        self.screen = pygame.display.set_mode(size)
 
-cell_size = 20
-grid_position = (0, 0)
-grid_size = (30, 30)
 
-start = (0, 0)
-end = (29, 29)
+    def run_app(self):
+        self.empty_cells = self.setup_grid()
 
-def setup_grid():
-    grid = []
-    for i in range(grid_size[1]):
-        row = []
-        for j in range(grid_size[0]):
-            rect = pygame.Rect(
-                grid_position[0] + j * cell_size,
-                grid_position[1] + i * cell_size,
-                cell_size, cell_size
-            )
-            pygame.draw.rect(screen, white, rect, 1)
-            row.append(rect)
-        grid.append(row)
+        while True:
+            mouse = pygame.mouse.get_pos()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    for button in self.buttons:
+                        if self.buttons[button].collidepoint(mouse):
+                            if button == "Solve":
+                                maze = Maze(self.grid_size, self.start, self.end, self.wall_indices)
+                                self.path, self.count = maze.solve_maze()
+                                print("length of path", len(self.path))
+                                if self.path:
+                                    self.path = self.indices_to_rects(self.path)
 
-    return grid
+                                print(self.count)
 
-def draw_maze(empty_cells, filled_cells):
-    for i in range(grid_size[1]):
-        row = []
-        for j in range(grid_size[0]):
-            if empty_cells[i][j]:
+                            elif button == "Clear":
+                                self.clear_grid()
+                            elif button == "Grid Size":
+                                _settings = large_settings if self.grid_size == [20, 20] else medium_settings
+                                print("changing size")
+                                self.change_grid_size(_settings)
+                                self.clear_grid()
+
+            self.screen.fill(black)
+
+            self.draw_maze()
+            self.buttons = self.draw_options()
+
+            left_click, _, right_click = pygame.mouse.get_pressed()
+
+            if left_click:
+                for i in range(self.grid_size[1]):
+                    for j in range(self.grid_size[0]):
+                        cell = self.empty_cells[i][j]
+                        if cell and cell.collidepoint(mouse):
+                            if cell not in self.filled_cells:
+                                self.filled_cells.append(cell)
+                                self.wall_indices.append((int(cell.x/self.cell_size), int(cell.y/self.cell_size)))
+                                cell = None
+            elif right_click:
+                for cell in self.filled_cells:
+                    if cell.collidepoint(mouse):
+                        grid_index = (int(cell.x/self.cell_size), int(cell.y/self.cell_size))
+                        new_rect = pygame.Rect(cell.x, cell.y, self.cell_size, self.cell_size)
+                        self.filled_cells.remove(cell)
+                        self.empty_cells[grid_index[1]][grid_index[0]] = new_rect
+                        self.wall_indices.remove(grid_index)
+
+            pygame.display.flip()
+
+    def clear_grid(self):
+        self.empty_cells = self.setup_grid()
+        self.wall_indices = []
+        self.filled_cells = []
+        self.path = []
+
+    def change_grid_size(self, size_settings):
+        self.cell_size = size_settings[0]
+        self.grid_size = size_settings[1]
+        self.start = [0, 0]
+        self.end = [x-1 for x in size_settings[1]]
+
+    def setup_grid(self):
+        grid = []
+        for i in range(self.grid_size[1]):
+            row = []
+            for j in range(self.grid_size[0]):
                 rect = pygame.Rect(
-                    grid_position[0] + j * cell_size,
-                    grid_position[1] + i * cell_size,
-                    cell_size, cell_size
+                    grid_position[0] + j * self.cell_size,
+                    grid_position[1] + i * self.cell_size,
+                    self.cell_size, self.cell_size
                 )
-                pygame.draw.rect(screen, white, rect, 1)
+                pygame.draw.rect(self.screen, white, rect, 1)
+                row.append(rect)
+            grid.append(row)
 
-    for cell in filled_cells:
-        pygame.draw.rect(screen, highlight, cell)
+        return grid
 
-    start_cell = pygame.Rect(start[0] * cell_size, start[1] * cell_size, cell_size, cell_size)
-    pygame.draw.rect(screen, start_color, start_cell)
-    end_cell = pygame.Rect(end[0] * cell_size, end[1] * cell_size, cell_size, cell_size)
-    pygame.draw.rect(screen, end_color, end_cell)
-
-def draw_options():
-    options_position_x = grid_size[0] * cell_size
-    options_rect = pygame.Rect(
-                options_position_x,
-                0,
-                width - options_position_x,
-                height
-                )
-    pygame.draw.rect(screen, options_box, options_rect)
-
-    buttons = {}
-    button_names = ["Solve", "Clear"]
-
-    button_size = (160, 60)
-    button_top_margin = 20
-
-    for i, button_name in enumerate(button_names):
-
-        button_x = (width - options_position_x) / 2 + options_position_x - button_size[0] / 2
-        button = pygame.Rect(
-                    button_x,
-                    button_top_margin * i + i * button_size[1],
-                    button_size[0],
-                    button_size[1]
+    def draw_maze(self):
+        for i in range(self.grid_size[1]):
+            row = []
+            for j in range(self.grid_size[0]):
+                if self.empty_cells[i][j]:
+                    rect = pygame.Rect(
+                        grid_position[0] + j * self.cell_size,
+                        grid_position[1] + i * self.cell_size,
+                        self.cell_size, self.cell_size
                     )
+                    pygame.draw.rect(self.screen, white, rect, 1)
 
-        button_text = mediumFont.render(button_name, True, black)
-        text_rect = button_text.get_rect()
-        text_rect.center = button.center
-        pygame.draw.rect(screen, button_color, button)
-        screen.blit(button_text, text_rect)
+        for cell in self.path:
+            pygame.draw.rect(self.screen, path_color, cell)
 
-        buttons[button_name] = button
+        for cell in self.filled_cells:
+            pygame.draw.rect(self.screen, highlight, cell)
 
-    return buttons
+        start_cell = pygame.Rect(self.start[0] * self.cell_size, self.start[1] * self.cell_size, self.cell_size, self.cell_size)
+        pygame.draw.rect(self.screen, start_color, start_cell)
+        end_cell = pygame.Rect(self.end[0] * self.cell_size, self.end[1] * self.cell_size, self.cell_size, self.cell_size)
+        pygame.draw.rect(self.screen, end_color, end_cell)
 
-def clear_grid():
-    print("clearing grid")
+    def draw_options(self):
+        options_position_x = self.grid_size[0] * self.cell_size
+        options_rect = pygame.Rect(
+                    options_position_x,
+                    0,
+                    width - options_position_x,
+                    height
+                    )
+        pygame.draw.rect(self.screen, options_box, options_rect)
 
-def main():
-    empty_cells = setup_grid()
+        buttons = {}
+        button_names = ["Solve", "Clear", "Grid Size"]
 
-    filled_cells = []
+        button_size = (160, 60)
+        button_top_margin = 20
 
-    buttons = []
-    while True:
-        mouse = pygame.mouse.get_pos()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-            elif event.type == pygame.MOUSEBUTTONUP:
-                for button in buttons:
-                    if buttons[button].collidepoint(mouse):
-                        if button == "Solve":
-                            maze = Maze(grid_size, filled_cells)
-                            maze.solve_maze()
-                        elif button == "Clear":
-                            empty_cells = setup_grid()
-                            filled_cells = []
+        for i, button_name in enumerate(button_names):
 
-        screen.fill(black)
+            button_x = (width - options_position_x) / 2 + options_position_x - button_size[0] / 2
+            button = pygame.Rect(
+                        button_x,
+                        button_top_margin * i + i * button_size[1],
+                        button_size[0],
+                        button_size[1]
+                        )
 
-        draw_maze(empty_cells, filled_cells)
-        buttons = draw_options()
+            button_text = mediumFont.render(button_name, True, black)
+            text_rect = button_text.get_rect()
+            text_rect.center = button.center
+            pygame.draw.rect(self.screen, button_color, button)
+            self.screen.blit(button_text, text_rect)
 
-        left_click, _, right_click = pygame.mouse.get_pressed()
+            buttons[button_name] = button
 
-        if left_click:
-            for i in range(grid_size[1]):
-                for j in range(grid_size[0]):
-                    if empty_cells[i][j] and empty_cells[i][j].collidepoint(mouse):
-                        if empty_cells[i][j] not in filled_cells:
-                            filled_cells.append(empty_cells[i][j])
-                            empty_cells[i][j] = None
-        elif right_click:
-            for cell in filled_cells:
-                if cell.collidepoint(mouse):
-                    grid_index = (int(cell.x/cell_size), int(cell.y/cell_size))
-                    new_rect = pygame.Rect(cell.x, cell.y, cell_size, cell_size)
-                    filled_cells.remove(cell)
-                    empty_cells[grid_index[1]][grid_index[0]] = new_rect
+        return buttons
 
-        pygame.display.flip()
+    def indices_to_rects(self, indices):
+        rect_array = []
+        for index in indices:
+            rect_array.append(pygame.Rect(index[0] * self.cell_size, index[1] * self.cell_size, self.cell_size, self.cell_size))
+        return rect_array
+
+
 
 if __name__ == "__main__":
-    main()
+    app = MazeInterface()
+    app.run_app()
